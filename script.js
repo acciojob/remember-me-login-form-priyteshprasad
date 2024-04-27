97e6c4d79e27c90b2765276d2aa735345bf2b4ce
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-27 19:49:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+// Check is credentials are saved or not;
+// console.log("line 3",localStorage)
+if(localStorage.getItem('username') && localStorage.getItem('password')){
+	// show buttun for logging as existing customer
+	let existingBtn = document.createElement('button');
+	existingBtn.setAttribute('id', 'existing')
+	existingBtn.textContent = 'Login as existing user';
+	document.body.appendChild(existingBtn);
+
+	//add action/eventListner to existing btn
+	existingBtn.addEventListener('click', ()=>{
+		alert('Logged in as '+ localStorage.getItem('username'))
+	})
+}
+
+// add even listener to login form
+document.getElementById('loginform').addEventListener('submit', (event)=>{
+	event.preventDefault();
+	console.log(event)
+
+	// get the values
+	const username = document.getElementById('username').value;
+	const password = document.getElementById('password').value;
+	const rememberMe = document.getElementById('checkbox').value;
+	if(username.trim() !== "" && password.trim() !== ""){
+		if(rememberMe){
+		localStorage.setItem('username', username);
+		localStorage.setItem('password', password);
+		}else{
+			localStorage.removeItem('username')
+			localStorage.removeItem('password')
+		}
+		alert('Logged in as '+ username)
+	}
+	
+
+	
+})
