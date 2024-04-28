9b57a147b3ebac2d9dcad1d37f2ee1f251511b02
PRIYTESH PRASAD <61977852+priyteshprasad@users.noreply.github.com>
2024-04-28 08:28:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 //your JS code here. If required.
 // Check is credentials are saved or not;
 // console.log("line 3",localStorage)
-if(localStorage.getItem('username') && localStorage.getItem('password')){
-	// show buttun for logging as existing customer
-	let existingBtn = document.createElement('button');
-	existingBtn.setAttribute('id', 'existing')
-	existingBtn.textContent = 'Login as existing user';
-	document.body.appendChild(existingBtn);
 
-	//add action/eventListner to existing btn
-	existingBtn.addEventListener('click', ()=>{
-		alert('Logged in as '+ localStorage.getItem('username'))
-	})
+function showLoginUserBtn(){
+	if(localStorage.getItem('username') && localStorage.getItem('password')){
+		// show buttun for logging as existing customer
+		let existingBtn = document.createElement('button');
+		existingBtn.setAttribute('id', 'existing')
+		existingBtn.textContent = 'Login as existing user';
+		document.body.appendChild(existingBtn);
+	
+		//add action/eventListner to existing btn
+		existingBtn.addEventListener('click', ()=>{
+			alert('Logged in as '+ localStorage.getItem('username'))
+		})
+	}
 }
+showLoginUserBtn()
 
 // add even listener to login form
 document.getElementById('loginform').addEventListener('submit', (event)=>{
 	event.preventDefault();
-	console.log(event)
+	// console.log(event)
 
 	// get the values
 	const username = document.getElementById('username').value;
@@ -28,12 +32,11 @@ document.getElementById('loginform').addEventListener('submit', (event)=>{
 		localStorage.setItem('username', username);
 		localStorage.setItem('password', password);
 		}else{
-			localStorage.removeItem('username')
-			localStorage.removeItem('password')
+			localStorage.setItem('username', null)
+			localStorage.setItem('password', null)
 		}
 		alert('Logged in as '+ username)
 	}
-	
-
+	// showLoginUserBtn()
 	
 })
